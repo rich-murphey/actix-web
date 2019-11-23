f0b2434a08565ace6a121d42bc5dd1bd896fa47a
Travis CI User <travis@example.org>
2019-11-23 15:37:20 +0000

    Documentation upload

---
+++ implementors/futures_core/stream/trait.Stream.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["actix_web"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/futures-core/0.3.0/futures_core/stream/trait.Stream.html\" title=\"trait futures_core::stream::Stream\">Stream</a> for <a class=\"struct\" href=\"actix_web/web/struct.Payload.html\" title=\"struct actix_web::web::Payload\">Payload</a>",synthetic:false,types:["actix_web::types::payload::Payload"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
